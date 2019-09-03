4c099adf91cdef401902b3ddd4b6ec2d9434ca9e
Ryan Leckey <ryan@launchbadge.com>
2019-09-03 09:44:11 -0700

    Adjust for Vue

--- vue-typescript.js
+++ vue-typescript.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["@vue/typescript"],
+  extends: ["@vue/typescript", "./typescript.js"],
   rules: {}
 };
--- vue.js
+++ vue.js
@@ -1,6 +1,11 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["plugin:vue/recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/recommended",
+    "@vue/prettier",
+    "./index.js",
+    "./browser.js"
+  ],
   rules: {
     "vue/array-bracket-spacing": "error",
     "vue/arrow-spacing": "error",
