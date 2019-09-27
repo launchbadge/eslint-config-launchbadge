b7782af63a22be7eb83ca7c8eb079c8144f41206
Ryan Leckey <ryan@launchbadge.com>
2019-09-27 16:06:18 -0700

    Error on console.log in production

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
     extends: [ "eslint:recommended", "plugin:import/errors", "plugin:unicorn/recommended" ],
     settings: { "import/resolver": { webpack: {}}},
     rules: {
-        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 
         // Broken in eslint@6.x
