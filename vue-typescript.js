135011f4551a81e7f57ad373ca8c25cdfde49032
Ryan Leckey <ryan@launchbadge.com>
2019-11-12 09:45:39 -0800

    Fix for recent changes to typescript plugin

--- vue-typescript.js
+++ vue-typescript.js
@@ -1,12 +1,14 @@
 /* eslint-env node */
 module.exports = {
-    extends: [ "@vue/typescript", "./typescript.js" ],
+    extends: [ "./typescript.js" ],
     parser: require.resolve("vue-eslint-parser"),
     parserOptions: {
         parser: "@typescript-eslint/parser",
         sourceType: "module",
         ecmaFeatures: { jsx: true },
-        warnOnUnsupportedTypeScriptVersion: false
+        warnOnUnsupportedTypeScriptVersion: false,
+        project: "./tsconfig.json",
+        extraFileExtensions: [ ".vue" ]
     },
     rules: {}
 };
